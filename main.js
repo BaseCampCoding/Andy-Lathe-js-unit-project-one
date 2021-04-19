0b40741ebe569c19c8510b6d7f52ac18dd4f82e3
Lathe Ward <lward21@basecampcodingacademy.org>
2021-04-19 11:31:57 -0500

    Saving progress on basic reaction consditionals

---
+++ main.js
@@ -0,0 +1,16 @@
+let readyTimer = setInterval(fireTime, 1000)
+if (difficulty === 1){
+    // thing
+}
+if (difficulty === 2){
+    // thing
+}
+if (difficulty === 3){
+    // thing
+}
+
+function fireTime(playerClick){
+    if(playerClick === undefined){
+
+    }
+}
