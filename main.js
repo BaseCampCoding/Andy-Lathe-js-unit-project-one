dbfc8698059cb195604b03d1c30c1231d2f778bd
Lathe Ward <lward21@basecampcodingacademy.org>
2021-04-19 15:54:28 -0500

    Set basic logic with setTimeout for win/loss

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-let readyTimer = setInterval(fireTime, 1000)
+const fireButton = document.getElementById("myBtn");
+let testThing = document.getElementById("testp"); // This was tested in another file
 
-const fireButton = document.querySelector(//Andy's button\\)
+
+function goodFire(){
+    testThing.innerHTML = "You did it in time"; // Will change when victory conditional is set
+    clearTimeout(notQuickEnough);
+    done = true;
+}
+
+function badFire(){
+    testThing.innerHTML = "Better luck next time"; // Will change with loss conditional
+}
+
+let done = false;
+let notQuickEnough = setTimeout(badFire, 2000)
+
+fireButton.addEventListener("click", goodFire)
+
+
+
 if (difficulty === 1){
-    readyTimer = setInterval(fireTime, 1700)
+    readyTimer = setInterval(badFire, 1700)
 }
 if (difficulty === 2){
-    readyTimer = setInterval(fireTime, 1000)
+    readyTimer = setInterval(badFire, 1000)
 }
 if (difficulty === 3){
-    readyTimer = setInterval(fireTime, 400)
+    readyTimer = setInterval(badFire, 400)
 }
-
-
-
-function fireTime(playerClick){
-    if(playerClick === null){
-        return "loss"
-    }
-
-
-}
-
