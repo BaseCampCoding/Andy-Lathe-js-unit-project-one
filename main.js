784e034d8eaa6faa815d55684e50acf3a6bcf77d
Lathe Ward <lward21@basecampcodingacademy.org>
2021-04-20 08:57:09 -0500

    Set the display to say DRAW for when user needs to click

--- main.js
+++ main.js
@@ -5,30 +5,39 @@ let testThing = document.getElementById("testp");
 readyButton.addEventListener("click", fireTime)
 
 function fireTime(){
+
+    testThing.innerHTML = "Hold"
     function goodFire(){
         testThing.innerHTML = "You did it in time"; // Will change when victory conditional is set
         clearTimeout(notQuickEnough);
         done = true;
     }
 
+    function drawTime(){
+        testThing.innerHTML = "DRAW"
+    }
+
     function badFire(){
-        testThing.innerHTML = "Better luck next time"; // Will change with loss conditional
+        testThing.innerHTML = "Better luck next time. Click ready to play again."; // Will change with loss conditional
     }
 
     let done = false;
+    let drawNow = setTimeout(drawTime, 1000)
     let notQuickEnough = setTimeout(badFire, 2000)
 
     fireButton.addEventListener("click", goodFire)
 
-
-
     if (difficulty === 1){
-        readyTimer = setInterval(badFire, 1700)
+        notQuickEnough = setTimeout(badFire, 2000)
+        drawNow = setTimeout(drawTime, 1000)
+
     }
     if (difficulty === 2){
-        readyTimer = setInterval(badFire, 1000)
+        notQuickEnough = setTimeout(badFire, 1000)
+        drawNow = setTimeout(drawTime, 600)
     }
     if (difficulty === 3){
-        readyTimer = setInterval(badFire, 400)
+        notQuickEnough = setTimeout(badFire, 400)
+        drawNow = setTimeout(drawTime, 300)
     }
 }
