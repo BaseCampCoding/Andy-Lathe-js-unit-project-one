d9cad2cd0746314a3fac528cd76eec811cad69aa
Lathe Ward <lward21@basecampcodingacademy.org>
2021-04-20 09:53:39 -0500

    Commented out difficulty, working on score addition

--- main.js
+++ main.js
@@ -21,28 +21,29 @@ function fireTime(){
 
     if(! badFire || done === false){
     function goodFire(){
-        testThing.innerHTML = "You did it in time"; // Will change when victory conditional is set
+        testThing.innerHTML = "You did it in time";
         clearTimeout(notQuickEnough);
-        score++
-        scoreCard.innerHTML = `Score: ${score}`
+        score += 1
+        console.log(score)
     }}
+    
+    let drawNow = setTimeout(drawTime, 1000);
+    let notQuickEnough = setTimeout(badFire, 2000);
+    scoreCard.innerHTML = `Score: ${score}`;
 
-    let drawNow = setTimeout(drawTime, 1000)
-    let notQuickEnough = setTimeout(badFire, 2000)
-
+    
+    // if (difficulty === 1){
+    //     notQuickEnough = setTimeout(badFire, 2000)
+    //     drawNow = setTimeout(drawTime, 1000)
+        
+    // }
+    // if (difficulty === 2){
+    //     notQuickEnough = setTimeout(badFire, 1000)
+    //     drawNow = setTimeout(drawTime, 600)
+    // }
+    // if (difficulty === 3){
+    //     notQuickEnough = setTimeout(badFire, 400)
+    //     drawNow = setTimeout(drawTime, 300)
+    // }
     fireButton.addEventListener("click", goodFire)
-
-    if (difficulty === 1){
-        notQuickEnough = setTimeout(badFire, 2000)
-        drawNow = setTimeout(drawTime, 1000)
-
-    }
-    if (difficulty === 2){
-        notQuickEnough = setTimeout(badFire, 1000)
-        drawNow = setTimeout(drawTime, 600)
-    }
-    if (difficulty === 3){
-        notQuickEnough = setTimeout(badFire, 400)
-        drawNow = setTimeout(drawTime, 300)
-    }
 }
