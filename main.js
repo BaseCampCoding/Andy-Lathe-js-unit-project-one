e61ab7b5e34a840751a9ee9133697a88cfdb8717
Lathe Ward <lward21@basecampcodingacademy.org>
2021-04-20 16:14:35 -0500

    PAin

--- main.js
+++ main.js
@@ -9,27 +9,29 @@ let scoreAmount = 0
 function fireTime(){
     let done = false
     testThing.innerHTML = "Hold"
-    
-    function drawTime(){
-        testThing.innerHTML = "DRAW"
+
+function drawTime(){
+    testThing.innerHTML = "DRAW"
+}
+setTimeout(drawTime, 1000); // After 1 second, users will have 1 second to draw
+
+function badFire(){
+    testThing.innerHTML = "Better luck next time. Click ready to play again.";
+    done = true
+}
+
+const tooLate = setTimeout(badFire, 2000); // If user doesn't hit in time, bad message, with done preventing scoring
+
+if(done === false){
+    function goodFire(){
+        testThing.innerHTML = "You did it! Click ready to play again";
+        clearTimeout(tooLate);
+        scoreAmount += 1
+        console.log(scoreAmount)
+        scoreCard.innerHTML = `Score: ${scoreAmount}`
+        
     }
-    setTimeout(drawTime, 1000); // After 1 second, users will have 1 second to draw
-
-    function badFire(){
-        testThing.innerHTML = "Better luck next time. Click ready to play again.";
-        done = true
-    }
-
-    const tooLate = setTimeout(badFire, 2000); // If user doesn't hit in time, bad message, with done preventing scoring
-
-    if(done === false){
-        function goodFire(){
-            testThing.innerHTML = "You did it! Click ready to play again";
-            clearTimeout(tooLate);
-            scoreAmount ++
-            scoreCard.textContent = `Score: ${scoreAmount}`
-        }
-        fireButton.addEventListener("click", goodFire)
+    fireButton.addEventListener("click", goodFire)
     }
     
 
