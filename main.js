df64634981fdece3be5ef1aae460e40883e8c312
Lathe Ward <lward21@basecampcodingacademy.org>
2021-04-21 14:58:56 -0500

    Brought script from index to main.js

--- main.js
+++ main.js
@@ -4,6 +4,109 @@ const testThing = document.getElementById("testp");
 const scoreCard = document.getElementById("thescore");
 const resetScore = document.getElementById("resetButton");
 
+   
+let myGamePiece;
+let myGamePiece2
+
+function startGame() {
+myGamePiece = new component(200, 120, "cowboyStart1.png", 10, 120, "image");
+myGamePiece2 = new component(200,120, "samuraiStart.png",230,120,"image2")
+myGameArea.start();
+}
+
+let myGameArea = {
+canvas : document.createElement("canvas"),
+start : function() {
+    this.canvas.width = 380;
+    this.canvas.height = 270;
+    this.canvas.style.left = "450px";
+    this.canvas.style.top = "100px";
+    this.canvas.style.position = "absolute";
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.frameNo = 0;
+    this.interval = setInterval(updateGameArea, 20);
+    },
+clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+},
+stop : function() {
+    clearInterval(this.interval);
+}
+}
+
+function component(width, height, color, x, y, type) {
+this.type = type;
+if (type == "image" || type == "image2") {
+    this.image = new Image();
+    this.image.src = color;
+}
+this.width = width;
+this.height = height;
+this.speedX = 0;
+this.speedY = 0;    
+this.x = x;
+this.y = y;    
+this.update = function() {
+    ctx = myGameArea.context;
+    if (type == "image" || type == "image2") {
+        ctx.drawImage(this.image, 
+            this.x, 
+            this.y,
+            this.width, this.height);
+    } else {
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+this.newPos = function() {
+    this.x += this.speedX;
+    this.y += this.speedY;        
+}
+}
+
+function updateGameArea() {
+myGameArea.clear();
+myGamePiece2.newPos();
+myGamePiece2.update();
+myGamePiece.newPos();
+myGamePiece.update();
+}
+
+
+var sampleVar;
+
+function sampleFunction(){    
+sampleVar = setTimeout(alertFunc, 2000);
+}
+
+function alertFunc(){    
+myGamePiece2.image.src = "samuraiStrike.png"
+
+}
+
+
+function move(dir) {
+//    myGamePiece2.image.src = "samuraiStrike.png"
+if (dir == "up") {
+myGamePiece.image.src = "cowboyShoot1.png";
+}
+
+ 
+ 
+
+}
+
+function clearmove(dir) {
+if(dir == "ready"){
+myGamePiece.image.src = "cowboyStart1.png";
+myGamePiece2.image.src = "samuraiStart.png"
+}
+
+
+
+}
+
 
 
 readyButton.addEventListener("click", fireTime)
