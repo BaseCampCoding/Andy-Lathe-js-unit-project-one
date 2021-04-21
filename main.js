25a622552fc0eede5eb3680aea90e106040a05ab
Lathe Ward <lward21@basecampcodingacademy.org>
2021-04-21 10:30:28 -0500

    added if statements to goodfire according to the testThing inner HTML

--- main.js
+++ main.js
@@ -10,34 +10,39 @@ let scoreAmount = 0
 resetScore.addEventListener("click", resetTheScore)
     function resetTheScore(){
         scoreAmount = 0
-        console.log(scoreAmount)
         scoreCard.innerHTML = `Score: ${scoreAmount}`
     }
 function fireTime(){
     let done = false
     testThing.innerHTML = "Hold"
 
-function drawTime(){
-    testThing.innerHTML = "DRAW"
-}
-setTimeout(drawTime, 1000); // After 1 second, users will have 1 second to draw
-
-function badFire(){
-    testThing.innerHTML = "Better luck next time. Click ready to play again.";
-    done = true
-}
-
-var tooLate = setTimeout(badFire, 2000); // If user doesn't hit in time, bad message, with done preventing scoring
-
-if(done === false){
-    function goodFire(){
-        testThing.innerHTML = "You did it! Click ready to play again";
-        clearTimeout(tooLate);
-        scoreCard.innerHTML = `Score: ${scoreAmount}`
-        
+    function drawTime(){
+        testThing.innerHTML = "DRAW"
     }
-    fireButton.addEventListener("click", goodFire)
-}
+    const drawTimeout = setTimeout(drawTime, 1000); // After 1 second, users will have 1 second to draw
+
+    function badFire(){
+        testThing.innerHTML = "Better luck next time. Click ready to play again.";
+        done = true
+    }
+
+    var tooLate = setTimeout(badFire, 2000); // If user doesn't hit in time, bad message, with done preventing scoring
+
+    if(done === false){
+        function goodFire(){
+            clearTimeout(tooLate);
+            clearTimeout(drawTimeout)
+            if(testThing.innerHTML === "DRAW"){
+                testThing.innerHTML = "You did it! Click ready to play again";
+                scoreCard.innerHTML = `Score: ${scoreAmount}`
+            }
+            else if(testThing.innerHTML === "Hold"){
+                testThing.innerHTML = "Too soon! Click ready to try again"
+            }
+            
+        }
+        fireButton.addEventListener("click", goodFire)
+    }
     
     if(goodFire){
         scoreAmount += 1
